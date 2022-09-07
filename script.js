1eab3f317ed7f2ed12b6103b216b968e22916404
Spartaf <101091867+Spartaf@users.noreply.github.com>
2022-09-07 23:17:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,15 +15,22 @@ menuHamburger.addEventListener('click',()=> (Headertitle.classList.toggle('littl
 const hr_tags = document.getElementsByClassName("seperate_pages")
 const title_Pages = document.getElementsByClassName("title_pages")
 const title_Header = document.querySelector(".logo")
-
-
+const logo_ln = document.querySelector(".ph_ln")
+const logo_insta = document.querySelector(".ph_insta")
 
 window.addEventListener('scroll' , () => {
-
     const {scrollTop, clientHeight} = document.documentElement;
-
     
+        const topPh_InstaToTopViewport = logo_insta.getBoundingClientRect().top;
 
+        if(scrollTop > (scrollTop + topPh_InstaToTopViewport).toFixed() - clientHeight * 1){
+            logo_insta.classList.add("fade_In_Left")
+            logo_ln.classList.add("fade_In_Right")      
+        }
+})
+
+window.addEventListener('scroll' , () => {
+    const {scrollTop, clientHeight} = document.documentElement;
     for (var i = 0; i < hr_tags.length; i++) {
         const hr = hr_tags[i]; // i-ème élément du tableau d'éléments
         const title = title_Pages[i]
@@ -43,11 +50,5 @@ window.addEventListener('scroll' , () => {
             title_Header.classList.add("supp_logo")
         }
         
-    }
-
-    
-    
-    
-
-    
+    }  
 })
